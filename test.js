460e8a8d7c6a0bb930eb7a638d9cfebc5283c5d7
sudiptanandan85 <93093852+sudiptanandan85@users.noreply.github.com>
2022-07-28 08:15:03 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,4 @@
 <script runat=server>
 Platform.Load('Core','1.1')
-try {
-  const PI = 3.141592653589793;
-  PI = 3.14;
-}
-catch (err) {
-  document.getElementById("demo").innerHTML = err;
-}
+write('1,2,3 Testing')
 </script>
