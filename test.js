39ec4ac634d4c9ade9cbf5b81ab16883446f7d6f
sudiptanandan85 <93093852+sudiptanandan85@users.noreply.github.com>
2022-07-28 08:30:17 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,7 @@
-<script runat="server">
-  Platform.Load('Core', '1.1')
-write('1,2,3 Testing')
+<script>
+let x = 123;
+document.getElementById("demo").innerHTML =
+  x.toString() + "<br>" +
+   (123).toString() + "<br>" +
+   (100 + 23).toString();
 </script>
