b5ffa9b5f1c97894619668e456734bc174a8c706
sudiptanandan85 <93093852+sudiptanandan85@users.noreply.github.com>
2022-07-28 07:38:14 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+<script runat=server>
+Platform.Load('Core','1.1')
+try {
+  const PI = 3.141592653589793;
+  PI = 3.14;
+}
+catch (err) {
+  document.getElementById("demo").innerHTML = err;
+}
+</script>
