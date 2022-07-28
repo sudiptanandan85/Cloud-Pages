57a5e030caad5c6b1f664d7be5ac2b9b82101ee7
sudiptanandan85 <93093852+sudiptanandan85@users.noreply.github.com>
2022-07-28 11:22:06 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,3 @@
 <script>
-var x = 5;
-var y = 6;
-var z = x + y;
-document.getElementById("demo").innerHTML =
-"The value of z is: " + z;
+document.getElementById("demo").innerHTML = "My First JavaScript";
 </script>
